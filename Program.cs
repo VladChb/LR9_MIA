b94324f4222eb3b44746c3d00c9e35b9c68efae3
VladuslavChabaniuk <chvladik07@gmail.com>
2025-11-20 09:27:51 +0200

    6

--- Program.cs
+++ Program.cs
@@ -92,4 +92,5 @@ app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
-app.Run();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
+app.Run($"http://0.0.0.0:{port}");
