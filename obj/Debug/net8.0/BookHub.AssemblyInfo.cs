c2304e475fc3e11ab0dd938ea46cc1e2285ce028
VladuslavChabaniuk <chvladik07@gmail.com>
2025-11-20 13:27:58 +0200

    19

--- obj/Debug/net8.0/BookHub.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookHub.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+562c77825ab71b1eec8b8afeb0ae0a27d60a36a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f3db5e6059f9d82a12d2262175586e95c590622")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
